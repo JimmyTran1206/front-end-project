bbb3913112b23de1ea15c85d7712040b206b7807
Jimmy Tran <quantrannhu@gmail.com>
2025-06-03 04:58:51 +0000

    fix the js imports with tsc-esm-fix

--- js/src/cart/toggleCart.js
+++ js/src/cart/toggleCart.js
@@ -1,28 +1,23 @@
-import { getElement } from '../utils';
-
-const cartOverlay = getElement('.cart-overlay') as HTMLElement;
-const closeCartBtn = getElement('.cart-close') as HTMLElement;
-const toggleCartBtn = getElement('.toggle-cart') as HTMLElement;
-
+import { getElement } from '../utils.js';
+const cartOverlay = getElement('.cart-overlay');
+const closeCartBtn = getElement('.cart-close');
+const toggleCartBtn = getElement('.toggle-cart');
 // cart button
 toggleCartBtn.addEventListener('click', () => {
   cartOverlay.classList.add('show');
 });
-
 // close cart button
 closeCartBtn.addEventListener('click', () => {
   cartOverlay.classList.remove('show');
 });
-
 // close cart if click outside the cart area
 cartOverlay.addEventListener('click', (e) => {
-  const eventTarget = e.target as HTMLElement;
+  const eventTarget = e.target;
   const insideCartArea = eventTarget.closest('aside.cart');
   if (!insideCartArea) {
     cartOverlay.classList.remove('show');
   }
 });
-
-export function openCart(): void {
+export function openCart() {
   cartOverlay.classList.add('show');
 }
